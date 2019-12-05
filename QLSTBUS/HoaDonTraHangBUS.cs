a51ad8a28da8055d3f50efcf952d9e979ff81508
linhmiyano99 <32361868+linhmiyano99@users.noreply.github.com>
2019-12-05 15:07:04 +0700

    update quan ly ban hang

---
+++ QLSTBUS/HoaDonTraHangBUS.cs
@@ -0,0 +1,36 @@
+﻿using QLSTDAL;
+using QLSTDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QLSTBUS
+{
+    class HoaDonTraHangBUS
+    {
+        private HoaDonTraHangDAL hdDAL;
+        public HoaDonTraHangBUS()
+        {
+            this.hdDAL = new HoaDonTraHangDAL();
+        }
+
+        public bool suaHoaDonTraHang(HoaDonTraHangDTO hoaDonTraHangDto)
+        {
+            bool re = this.hdDAL.sua(hoaDonTraHangDto);
+            return true;
+        }
+        public bool themHoaDonTraHang(HoaDonTraHangDTO hoaDonTraHangDto)
+        {
+            bool re = this.hdDAL.them(hoaDonTraHangDto);
+            return true;
+        }
+        public bool xoaHoaDonTraHang(HoaDonTraHangDTO hoaDonTraHangDto)
+        {
+            bool re = this.hdDAL.xoa(hoaDonTraHangDto);
+            return true;
+        }
+    }
+}
